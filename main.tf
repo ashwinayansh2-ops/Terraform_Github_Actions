f20b219f8d2936d9c1a90e7ad26ab907ccb3fe2e
shashi <ds_talluri@yahoo.com>
2026-07-20 03:38:43 +0530

    Test

--- main.tf
+++ main.tf
@@ -18,3 +18,22 @@ resource "aws_subnet" "public_subnet_1b" {
   availability_zone = "us-east-1b"
 }
 
+resource "aws_instance" "ec2_instance_1" {
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnet_1a.id
+  tags = {
+    Name = "CICD_EC2_Instance_1"
+  } 
+  
+}
+
+resource "aws_instance" "ec2_instance_2" {
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnet_1b.id
+  tags = {
+    Name = "CICD_EC2_Instance_2"
+  } 
+  
+}
