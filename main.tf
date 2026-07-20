6dae42669f66c77538e249fe79c12246c0f240e5
shashi <ds_talluri@yahoo.com>
2026-07-20 05:42:08 +0530

    ASG1

--- main.tf
+++ main.tf
@@ -166,7 +166,12 @@ resource "aws_autoscaling_group" "dynatrace_asg" {
     version = "$Latest"
   }
 
-  health_check_type         = "EC2"
+  target_group_arns = [
+    aws_lb_target_group.dynatrace_tg.arn
+  ]
+
+  health_check_type = "ELB"
+
   health_check_grace_period = 300
 
   tag {
@@ -175,3 +180,63 @@ resource "aws_autoscaling_group" "dynatrace_asg" {
     propagate_at_launch = true
   }
 }
+
+resource "aws_lb_target_group" "dynatrace_tg" {
+  name     = "dynatrace-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.vpc.id
+
+  health_check {
+    enabled             = true
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name = "Dynatrace-TG"
+  }
+}
+
+
+resource "aws_lb" "dynatrace_alb" {
+
+  name               = "dynatrace-alb"
+  internal           = false
+  load_balancer_type = "application"
+
+  security_groups = [
+    aws_security_group.ec2_sg.id
+  ]
+
+  subnets = [
+    aws_subnet.public_subnet_1a.id,
+    aws_subnet.public_subnet_1b.id
+  ]
+
+  tags = {
+    Name = "Dynatrace-ALB"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+
+  load_balancer_arn = aws_lb.dynatrace_alb.arn
+
+  port     = 80
+  protocol = "HTTP"
+
+  default_action {
+
+    type = "forward"
+
+    target_group_arn = aws_lb_target_group.dynatrace_tg.arn
+  }
+}
+
+
